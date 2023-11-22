712b05683fdf680eb035716ef651daee8dde895e
DeviJv <septian.ardi.wijaya@gmail.com>
2023-11-22 14:01:47 +0700

    asset icon
    put in to folder backend/components/

---
+++ icons/arrowDown.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ArrowDownIcon() {
+    return (
+        <svg className='ml-1 mr-1' width="17" height="17" viewBox="0 0 17 17" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M10.6663 6.66666L7.99967 9.33332L5.33301 6.66666" stroke="#2B3241" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
---
+++ icons/subsIcon.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SubsIcon() {
+    return (
+        <svg width="20" height="21" viewBox="0 0 20 21" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M13.124 13.625H15.624M9.37402 13.625H10.624M1.87402 8.06658H18.124M2.49948 4.875H17.4995C17.8447 4.875 18.1245 5.15482 18.1245 5.5V15.5C18.1245 15.8452 17.8447 16.125 17.4995 16.125H2.49948C2.1543 16.125 1.87448 15.8452 1.87448 15.5V5.5C1.87448 5.15482 2.1543 4.875 2.49948 4.875Z" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" />
+        </svg>
+    )
+}
