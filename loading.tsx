d0c56eebc0cf54e1a3de23f02a2db1bd50988560
DeviJv <septian.ardi.wijaya@gmail.com>
2023-11-23 13:36:27 +0700

    loading animation grid
    put into folder templates

---
+++ loading.tsx
@@ -0,0 +1,78 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+export default function Loading() {
+    return (
+        <>
+            <div className='mt-5 flex items-center justify-between mx-auto container animate-pulse'>
+                <div className='space-x-2'>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                </div>
+                <div className='flex items-center space-x-6'>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[80px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[30px] h-[40px]'>
+                        <div className='border  px-4 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                    <button className='font-medium text-xs text-avatar-gray-500 leading-5 w-[30px] h-[40px]'>
+                        <div className='border  px-2 py-3 rounded-full bg-gray-200 border-avatar-border-50'>
+
+                        </div>
+                    </button>
+                </div>
+            </div>
+            <div className="grid grid-cols-4 gap-4 mx-auto container mt-5 animate-pulse">
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+                <div className='border w-[244px] h-[188px] bg-gray-200 rounded-xl p-4  hover:shadow-sm'></div>
+
+            </div>
+        </>
+    )
+}
